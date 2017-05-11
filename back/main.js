b757358f225ed5e766219a5028ab6a3d72f72aac
tjabejohannes <tjabe_johannes@outlook.com>
2017-05-11 20:59:41 +0200

    making some (more) changes to the ground structure

--- back/main.js
+++ back/main.js
@@ -18,7 +18,7 @@ function createWindow () {
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, '/../front/index.html'),
         protocol: 'file:',
         slashes: true
     }))
