db3fb7c6ac6ed85d37d531c3dc7a6bc3e321389f
tjabejohannes <tjabe_johannes@outlook.com>
2017-06-06 13:47:16 +0200

    Setting up basics for frontend

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ First electron app
 ## TODO
 - [X] Make electron run
 - [X] Structure basic Front Back End
-- [ ] 
+- [ ] Make a better readme :P 
---
+++ front/Button.js
@@ -0,0 +1,13 @@
+/**
+ * Created by LittleGpNator on 06/06/2017.
+ */
+
+
+function addNewClipboardlink() {
+    console.log("hallo");
+    $('#Column').append('<div class="Box"><div class="RightColumn"><button class="edit">Edit</button><div class="addtoclipboard" id="addtoclipboardButton"><button class="add">Add</button></div></div><div class="LeftColumn"> The Name </div></div>');
+};
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
